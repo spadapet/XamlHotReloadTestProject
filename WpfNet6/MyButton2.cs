f7a4088dc6782a528b19c66f1a9d990b31565c80
Peter Spada <spadapet@hotmail.com>
2021-08-16 14:31:41 -0700

    Add MyButton2

---
+++ WpfNet6/MyButton2.cs
@@ -0,0 +1,37 @@
+﻿using System.Diagnostics;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfNet6
+{
+    //public class MyButton2 : Button
+    //{
+    //    public static readonly DependencyProperty Foo1Property = DependencyProperty.Register(
+    //        "Foo1", typeof(string), typeof(MyButton2), new PropertyMetadata("Foo1 Default", MyButton2.Foo1Changed));
+    //
+    //    public string Foo1
+    //    {
+    //        get => (string)this.GetValue(MyButton2.Foo1Property);
+    //        set => this.SetValue(MyButton2.Foo1Property, value);
+    //    }
+    //
+    //    public static void Foo1Changed(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs args)
+    //    {
+    //        Debug.Assert(args.Property == MyButton2.Foo1Property);
+    //    }
+    //
+    //    //public static readonly DependencyProperty Foo2Property = DependencyProperty.Register(
+    //    //    "Foo2", typeof(string), typeof(MyButton2), new PropertyMetadata("Foo2 Default", MyButton2.Foo2Changed));
+    //    //
+    //    //public string Foo2
+    //    //{
+    //    //    get => (string)this.GetValue(MyButton2.Foo2Property);
+    //    //    set => this.SetValue(MyButton2.Foo2Property, value);
+    //    //}
+    //    //
+    //    //public static void Foo2Changed(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs args)
+    //    //{
+    //    //    Debug.Assert(args.Property == MyButton2.Foo2Property);
+    //    //}
+    //}
+}
