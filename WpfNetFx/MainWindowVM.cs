92d4562507ec7a56ee14d324049c7645375c5adc
Peter Spada <spadapet@hotmail.com>
2021-08-16 15:34:45 -0700

    Use proper delegate methods for Add ICommand

--- WpfNetFx/MainWindowVM.cs
+++ WpfNetFx/MainWindowVM.cs
@@ -28,23 +28,27 @@ namespace WpfNetFx
             });
         }
 
-        public ICommand AddCommand => new DelegateCommand(p =>
+        public ICommand AddCommand => new DelegateCommand(this.Add);
+
+        private void Add()
         {
             this.People.Add(new Person()
             {
                 Name = "New Person",
                 Age = App.Random.Next(20, 40)
             });
-        });
+        }
 
-        //public ICommand AddCommand2 => new DelegateCommand(p =>
+        //public ICommand AddCommand2 => new DelegateCommand(this.Add2);
+
+        //private void Add2()
         //{
         //    this.People.Add(new Person()
         //    {
-        //        Name = "New Person2",
+        //        Name = "New Person 2",
         //        Age = System.Random.Shared.Next(40, 60)
         //    });
-        //});
+        //}
 
         public ICommand ClearCommand => new DelegateCommand(p =>
         {
